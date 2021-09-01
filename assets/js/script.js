8943a2ef3d9f335dccd3a9df1d7f98c1d8fb696b
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-01 10:39:42 -0700

    create question variable

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// Array of questions for quiz
+var questions = [
+    {
+        question: "How do you display an alert box with the words \"Coding Quiz\"?",
+        answers: {
+            1: "displayBox(\"Coding Quiz\")",
+            2: "writeAlert(\"Coding Quiz\")",
+            3: "alert(\"Coding Quiz\")",
+            4: "boxAlert(\"Coding Quiz\")"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "What do you use to enclose the variables in an array?",
+        answers: {
+            1: "Quotation marks \"\"",
+            2: "Curly brackets {}",
+            3: "Parenthesis ()",
+            4: "Square Brackets []"
+        },
+        correctAnswer: 4
+    },
+    {
+        question: "How do you call a function named \"thisFunction\"?",
+        answers: {
+            1: "thisFunction().call",
+            2: "thisFunction()",
+            3: "callFunction thisFunction",
+            4: "call thisFunction"
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "How do you write an if statement to run code if \"i\" does not equal 9?",
+        answers: {
+            1: "if i-=9",
+            2: "if (i=!9)",
+            3: "if i!=9",
+            4: "if (i!=9)"
+        },
+        correctAnswer: "4"
+        },
+    {
+        question: "Choose the correctly written array.",
+        answers: {
+            1: "var animals = [\"cats\", \"dogs\", \"horses\", \"cows\"]",
+            2: "var animals = [\"cats, dogs, horses, cows\"]",
+            3: "var animals = (\"cats, dogs, horses, cows\")",
+            4: "var animals = [cats dogs horses cows]"
+        },
+        correctAnswer: "1"
+        }
+];
+
+// variable to keep track of score
+var score = 0
+
+for (var i = 0; i <questions.length; i++) {
+    
+}
+
+// quiz function
+
+function 
