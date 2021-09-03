331cf23bfd970bbf82b36974666f44f6d26ae6f8
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-03 15:53:42 -0700

    add comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
+// global variables
 var timerInterval;
 
 var currentQuestion = 0;
-// variable to keep track of score
+
 var score = 0;
 
 var highScores = [];
 
 var timeLeft = 90;
 
+// global constants
 const timer = document.getElementById('timer');
 
 const formQuestion = document.getElementById('form-question');
@@ -42,12 +44,16 @@ const initials = document.getElementById('initials');
 
 const submitInitials = document.getElementById('submit-initials');
 
+const resetHighScores = document.getElementById('clear-scores');
+
+// function to end game
 function endGame() {
     formQuiz.style.display = "none";
     quizResult.style.display = "block";
     finalScore.textContent = score;
 }
 
+// reset timer after time reaches zero
 function resetTimer() {
     if (timerInterval) {
         clearInterval(timerInterval);
@@ -63,6 +69,7 @@ function resetTimer() {
     }, 1000);
 }
 
+// start quiz on button click
 startButton.addEventListener('click', function(event) {
     event.preventDefault();
     intro.style.display = "none";
@@ -70,13 +77,13 @@ startButton.addEventListener('click', function(event) {
     displayQuestion();
     resetTimer();
 });
-
+// view high scores by clicking link
 viewHighScores.addEventListener('click', function(event) {
     event.preventDefault();
     highScoreOverlay.style.display = "block";
     clearInterval(timerInterval);
 });
-
+//return to quiz from high score screen
 returnButton.addEventListener('click', function(event) {
     event.preventDefault();
     highScoreOverlay.style.display = "none";
@@ -96,48 +103,48 @@ var questions = [{
         },
         correctAnswer: 3
     },
-    // {
-    //     question: "What do you use to enclose the variables in an array?",
-    //     answers: {
-    //         1: "Quotation marks \"\"",
-    //         2: "Curly brackets {}",
-    //         3: "Parenthesis ()",
-    //         4: "Square Brackets []"
-    //     },
-    //     correctAnswer: 4
-    // },
-    // {
-    //     question: "How do you call a function named \"thisFunction\"?",
-    //     answers: {
-    //         1: "thisFunction().call",
-    //         2: "thisFunction()",
-    //         3: "callFunction thisFunction",
-    //         4: "call thisFunction"
-    //     },
-    //     correctAnswer: 2
-    // },
-    // {
-    //     question: "How do you write an if statement to run code if \"i\" does not equal 9?",
-    //     answers: {
-    //         1: "if i-=9",
-    //         2: "if (i=!9)",
-    //         3: "if i!=9",
-    //         4: "if (i!=9)"
-    //     },
-    //     correctAnswer: 4
-    // },
-    // {
-    //     question: "Choose the correctly written array.",
-    //     answers: {
-    //         1: "var animals = [\"cats\", \"dogs\", \"horses\", \"cows\"]",
-    //         2: "var animals = [\"cats, dogs, horses, cows\"]",
-    //         3: "var animals = (\"cats, dogs, horses, cows\")",
-    //         4: "var animals = [cats dogs horses cows]"
-    //     },
-    //     correctAnswer: 1
-    // }
+    {
+        question: "What do you use to enclose the variables in an array?",
+        answers: {
+            1: "Quotation marks \"\"",
+            2: "Curly brackets {}",
+            3: "Parenthesis ()",
+            4: "Square Brackets []"
+        },
+        correctAnswer: 4
+    },
+    {
+        question: "How do you call a function named \"thisFunction\"?",
+        answers: {
+            1: "thisFunction().call",
+            2: "thisFunction()",
+            3: "callFunction thisFunction",
+            4: "call thisFunction"
+        },
+        correctAnswer: 2
+    },
+    {
+        question: "How do you write an if statement to run code if \"i\" does not equal 9?",
+        answers: {
+            1: "if i-=9",
+            2: "if (i=!9)",
+            3: "if i!=9",
+            4: "if (i!=9)"
+        },
+        correctAnswer: 4
+    },
+    {
+        question: "Choose the correctly written array.",
+        answers: {
+            1: "var animals = [\"cats\", \"dogs\", \"horses\", \"cows\"]",
+            2: "var animals = [\"cats, dogs, horses, cows\"]",
+            3: "var animals = (\"cats, dogs, horses, cows\")",
+            4: "var animals = [cats dogs horses cows]"
+        },
+        correctAnswer: 1
+    }
 ];
-
+// display all possible answers as buttons
 function displayQuestion() {
     formQuestion.textContent = questions[currentQuestion].question;
     answer1.textContent = " 1. " + questions[currentQuestion].answers[1];
@@ -165,7 +172,7 @@ answer4.addEventListener('click', function(event) {
     event.preventDefault();
     showResult(4);
 });
-
+// display "correct" or "incorrect" based on which answer was clicked
 function showResult(answer) {
     result.style.display = "block";
     if (questions[currentQuestion].correctAnswer === answer) {
@@ -187,7 +194,7 @@ function showResult(answer) {
         }
     }, 3000);
 }
-
+//reset score and timer to restart quiz
 function resetQuiz() {
     quizResult.style.display = "none";
     formQuiz.style.display = "block";
@@ -199,7 +206,7 @@ function resetQuiz() {
     clearInterval(timerInterval);
     resetTimer();
 }
-
+// display high score list
 function renderHighScores() {
     var highScoreList = highScoreOverlay.querySelector("ol");
     highScoreList.innerHTML = "";
@@ -209,7 +216,7 @@ function renderHighScores() {
         highScoreList.appendChild(item);
     }
 }
-
+// prompt to enter user initials after game ends
 submitInitials.addEventListener('click', function(event) {
     event.preventDefault();
     var name = initials.value;
@@ -229,8 +236,3 @@ submitInitials.addEventListener('click', function(event) {
     renderHighScores();
     resetQuiz();
 });
-
-
-
-
-// start quiz
